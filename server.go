32f6e6a95c39d586250371d1fb19257d098fbed4
Dan Jacob <danjac354@gmail.com>
2014-05-15 15:40:20 +0300

    Code tidy

--- server.go
+++ server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"flag"
 	"github.com/coopernurse/gorp"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"log"
-    "flag"
 )
 
 func checkErr(err error) {
@@ -16,8 +16,8 @@ func checkErr(err error) {
 
 func main() {
 
-    dbName := flag.String("database", "/tmp/tweets.db", "Sqlite database name")
-    flag.Parse()
+	dbName := flag.String("database", "/tmp/tweets.db", "Sqlite database name")
+	flag.Parse()
 
 	// DATABASE
 	dbMap := initDb(*dbName)
