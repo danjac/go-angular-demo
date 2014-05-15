74d1d4db34bc6b1d4ca362269c8f6628edc1a8b2
Dan Jacob <danjac354@gmail.com>
2014-05-15 13:56:06 +0300

    Tidy up

--- server.go
+++ server.go
@@ -26,33 +26,33 @@ func (post *Post) Validate(errors *binding.Errors, req *http.Request) {
 	}
 }
 
+func checkErr (err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
 func initDb() (*gorp.DbMap, error) {
 	db, err := sql.Open("sqlite3", "/tmp/tweets.db")
-	if err != nil {
-		return nil, err
-	}
+    checkErr(err)
 
 	dbMap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
 	dbMap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
 	err = dbMap.CreateTablesIfNotExists()
-	if err != nil {
-		return nil, err
-	}
+    checkErr(err)
 	dbMap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 	return dbMap, nil
 }
 
-func getTweets(dbMap *gorp.DbMap, r render.Render) {
+func getTweets(db gorp.SqlExecutor, r render.Render) {
 	var posts []Post
-	_, err := dbMap.Select(&posts, "SELECT * FROM posts ORDER BY id DESC")
-	if err != nil {
-		log.Fatal(err)
-	}
+	_, err := db.Select(&posts, "SELECT * FROM posts ORDER BY id DESC")
+    checkErr(err)
 	r.JSON(http.StatusOK, posts)
 }
 
 func addTweet(
-	dbMap *gorp.DbMap,
+	db gorp.SqlExecutor,
 	post Post,
 	errors binding.Errors,
 	r render.Render) {
@@ -60,33 +60,27 @@ func addTweet(
 		r.JSON(http.StatusConflict, errors)
 		return
 	}
-	err := dbMap.Insert(&post)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err := db.Insert(&post)
+    checkErr(err)
 	r.JSON(http.StatusOK, post)
 }
 
 func deleteTweet(
-	dbMap *gorp.DbMap,
+	db gorp.SqlExecutor,
 	params martini.Params,
 	r render.Render) {
-	obj, err := dbMap.Get(Post{}, params["id"])
-	if err != nil {
-		log.Fatal(err)
-	}
+	obj, err := db.Get(Post{}, params["id"])
+    checkErr(err)
 	if obj == nil {
 		r.JSON(http.StatusNotFound, "NotFound")
 		return
 	}
-	_, err = dbMap.Delete(obj)
-	if err != nil {
-		log.Fatal(err)
-	}
+	_, err = db.Delete(obj)
+    checkErr(err)
 	r.JSON(http.StatusOK, "Deleted")
 }
 
-func addHandlers(m *martini.ClassicMartini) {
+func addRoutes(m *martini.ClassicMartini) {
 
 	m.Group("/api", func(r martini.Router) {
 		r.Get("", getTweets)
@@ -99,16 +93,15 @@ func main() {
 
 	// DATABASE
 	dbMap, err := initDb()
-	if err != nil {
-		log.Fatal(err)
-	}
+    checkErr(err)
 	defer dbMap.Db.Close()
 
 	// SERVER
 	m := martini.Classic()
 	m.Use(render.Renderer())
+	m.MapTo(dbMap, (*gorp.SqlExecutor)(nil))
 
-	m.Map(dbMap)
-	addHandlers(m)
+	addRoutes(m)
+
 	m.Run()
 }
