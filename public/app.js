9f130e3b3671db9281f7142d230e73d647024c19
Dan Jacob <danjac354@gmail.com>
2014-05-18 14:18:51 +0300

    content tracker fix

--- public/app.js
+++ public/app.js
@@ -31,20 +31,28 @@ angular.module('postApp', ['ngResource'])
         $interval(getPosts, 5000);
     }])
     .directive('contentLengthTracker', function () {
+        function isDangerZone(charsRemaining, maxLength) {
+            return charsRemaining < (maxLength / 10);
+        }
         return {
             link: function ($scope, element, attrs) {
-                $scope.$watch(attrs.content, function (value) {
-                    if ($scope.content) {
-                        $scope.charsRemaining = parseInt(attrs.maxlength, 10) - $scope.content.length;
-                        $scope.showWarning = $scope.charsRemaining < 10;
+                var maxLength = parseInt(attrs.maxlength, 10);
+                $scope.$watch(
+                    function () { return $scope.content; },
+                    function () {
+                        var content = $scope.content;
+                        if ($scope.content) {
+                            $scope.charsRemaining = maxLength - $scope.content.length;
+                            $scope.showWarning = isDangerZone($scope.charsRemaining, maxLength);
+                        }
                     }
-                });
+                );
             },
             restrict: 'E',
             replace: true,
             scope: {
-                content: '=',
-                maxlength: "="
+                content: '=content',
+                maxlength: "@"
             },
             template: '<div ng-show="content" class="bg-info" ng-class="{\'bg-danger\': showWarning}">{{charsRemaining}}</div>'
         };
