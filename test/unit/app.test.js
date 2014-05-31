ca35739b705b400730e6b92c958153872c6a9a5c
Dan Jacob <danjac354@gmail.com>
2014-05-31 16:31:51 +0300

    Karma tests passing

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -2,14 +2,56 @@
 
 describe('PostCtrl', function () {
     // mocking/DI
-    var scope;
+    var scope, $httpBackend;
 
     beforeEach(angular.mock.module('postApp'));
-    beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+
+    beforeEach(angular.mock.inject(function ($rootScope, $controller, _$httpBackend_) {
+
+        $httpBackend = _$httpBackend_;
+        $httpBackend.when("GET", "/api/").respond([
+            {
+                id: 1,
+                content: "test1"
+            },
+            {
+                id: 2,
+                content: "test2"
+            },
+            {
+                id: 3,
+                content: "test3"
+            }
+        ]);
+
+        $httpBackend.when("POST", "/api/").respond({
+            id: 4,
+            content: "test4"
+        });
+
+        $httpBackend.when("DELETE", "/api/1/").respond("Post deleted");
+
         scope = $rootScope.$new();
         $controller('PostCtrl', {$scope: scope});
+
     }));
-    it('should include some empty messages', function () {
-        expect(scope.posts.length).toBe(0);
+
+    it('should add a post', function () {
+        $httpBackend.flush();
+        scope.newPost.content = "test4";
+        scope.addPost();
+        expect(scope.posts.length).toBe(4);
+        expect(scope.newPost.content).toBe(undefined);
+    });
+
+    it('should delete a post', function () {
+        $httpBackend.flush();
+        scope.deletePost(scope.posts[0]);
+        expect(scope.posts.length).toBe(2);
+    });
+
+    it('should include some posts', function () {
+        $httpBackend.flush();
+        expect(scope.posts.length).toBe(3);
     });
 });
