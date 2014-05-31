0b21c5822fd446ff20fe16d1f0630fc0b7e17263
Dan Jacob <danjac354@gmail.com>
2014-05-31 19:41:39 +0300

    Directive tests

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,5 +1,50 @@
 'use strict';
 
+describe('ContentLengthTracker', function () {
+
+    var scope, compile;
+
+    function recompile(content) {
+        var element = angular.element("<p><content-length-tracker maxlength=\"20\" content=\"post.content\" /></p>");
+
+        scope.post = {'content': content};
+        compile(element)(scope);
+        scope.$digest();
+        return element;
+    }
+
+    beforeEach(angular.mock.module('postApp'));
+
+    beforeEach(angular.mock.inject(function ($rootScope, $compile) {
+        compile = $compile;
+        scope = $rootScope;
+    }));
+
+
+    it("should show how many characters remaining", function () {
+        var element = recompile("testing"),
+            div = element.find("div");
+        expect(div.text()).toBe("13 characters remaining");
+        expect(div.hasClass('ng-hide')).toBe(false);
+    });
+
+    it("should show a warning if few characters left", function () {
+        var element = recompile("tests the warning!"),
+            div = element.find("div");
+        expect(div.text()).toBe("2 characters remaining");
+        expect(div.hasClass('bg-danger')).toBe(true);
+        expect(div.hasClass('ng-hide')).toBe(false);
+    });
+
+    it("should be removed if content is empty", function () {
+        var element = recompile(""),
+            div = element.find("div");
+        expect(div.hasClass('ng-hide')).toBe(true);
+    });
+
+
+});
+
 describe('PostCtrl', function () {
     // mocking/DI
     var scope, $httpBackend;
